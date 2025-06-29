ee5b5ddb7b08a75125ff260ac21051e0104390ec
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2025-06-29 13:27:03 -0400

    - Fixed mod effecting end biomes.

--- src/main/java/com/Apothic0n/Winternal/mixin/BiomeMixin.java
+++ src/main/java/com/Apothic0n/Winternal/mixin/BiomeMixin.java
@@ -3,6 +3,7 @@ package com.Apothic0n.Winternal.mixin;
 import com.Apothic0n.Winternal.Winternal;
 import net.minecraft.world.level.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -10,17 +11,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Biome.class)
 public abstract class BiomeMixin {
 
+    @Shadow abstract Biome.BiomeCategory getBiomeCategory();
+
     @Inject(method = "getBaseTemperature", at = @At("HEAD"), cancellable = true)
     public void getBaseTemperature(CallbackInfoReturnable<Float> ci) {
         if (Winternal.enabled) {
-            ci.setReturnValue(-1f);
+            if (!this.getBiomeCategory().getName().contains("end")) {
+                ci.setReturnValue(-1f);
+            }
         }
     }
 
     @Inject(method = "isHumid", at = @At("HEAD"), cancellable = true)
     public void isHumid(CallbackInfoReturnable<Boolean> ci) {
         if (Winternal.enabled) {
-            ci.setReturnValue(true);
+            if (!this.getBiomeCategory().getName().contains("end")) {
+                ci.setReturnValue(true);
+            }
         }
     }
 }
