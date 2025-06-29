82a39a8dc87f95881d9ca1b08b2fbf095efb65d1
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2025-06-29 13:07:52 -0400

    - No longer overwrites vanilla methods.

--- src/main/java/com/Apothic0n/Winternal/mixin/BiomeMixin.java
+++ src/main/java/com/Apothic0n/Winternal/mixin/BiomeMixin.java
@@ -1,48 +1,26 @@
 package com.Apothic0n.Winternal.mixin;
 
 import com.Apothic0n.Winternal.Winternal;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Biome.class)
 public abstract class BiomeMixin {
 
-//    @Inject(method = "getBaseTemperature", at = @At("HEAD"), cancellable = true)
-//    public void getBaseTemperature(CallbackInfoReturnable<Float> ci) {
-//        ci.setReturnValue(-1f);
-//    }
-
-    @Final
-    @Shadow
-    private Biome.ClimateSettings climateSettings;
-
-    @Shadow public abstract float getDownfall();
-
-    /**
-     * @author Apothicon
-     * @reason Make temperature freezing everywhere.
-     */
-    @Overwrite
-    public final float getBaseTemperature() {
+    @Inject(method = "getBaseTemperature", at = @At("HEAD"), cancellable = true)
+    public void getBaseTemperature(CallbackInfoReturnable<Float> ci) {
         if (Winternal.enabled) {
-            return -1f;
+            ci.setReturnValue(-1f);
         }
-        return this.climateSettings.temperature;
     }
 
-    /**
-     * @author Apothicon
-     * @reason Make weather everywhere.
-     */
-    @Overwrite
-    public boolean isHumid() {
+    @Inject(method = "isHumid", at = @At("HEAD"), cancellable = true)
+    public void isHumid(CallbackInfoReturnable<Boolean> ci) {
         if (Winternal.enabled) {
-            return true;
+            ci.setReturnValue(true);
         }
-        return this.getDownfall() > 0.85f;
     }
 }
