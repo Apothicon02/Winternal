da8d0e3d72913a45c813545ea4042f93bc41189a
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2025-07-16 10:16:17 -0400

    - Updated to 1.1.0. - All surface water now freezes. - Fixed deserts & frozen oceans not snowing.

--- src/main/java/com/Apothic0n/Winternal/mixin/BiomeMixin.java
+++ src/main/java/com/Apothic0n/Winternal/mixin/BiomeMixin.java
@@ -1,7 +1,10 @@
 package com.Apothic0n.Winternal.mixin;
 
 import com.Apothic0n.Winternal.Winternal;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.material.Fluids;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,11 +16,38 @@ public abstract class BiomeMixin {
 
     @Shadow abstract Biome.BiomeCategory getBiomeCategory();
 
+    @Inject(method = "shouldFreeze(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;Z)Z", at = @At("HEAD"), cancellable = true)
+    public void shouldFreeze(LevelReader level, BlockPos pos, boolean p_47483_, CallbackInfoReturnable<Boolean> ci) {
+        if (Winternal.enabled) {
+            if (this.getBiomeCategory() != Biome.BiomeCategory.THEEND && level.getFluidState(pos).is(Fluids.WATER)) {
+                ci.setReturnValue(true);
+            }
+        }
+    }
+
     @Inject(method = "getBaseTemperature", at = @At("HEAD"), cancellable = true)
     public void getBaseTemperature(CallbackInfoReturnable<Float> ci) {
         if (Winternal.enabled) {
-            if (!this.getBiomeCategory().getName().contains("end")) {
+            if (this.getBiomeCategory() != Biome.BiomeCategory.THEEND) {
                 ci.setReturnValue(-1f);
+            }
+        }
+    }
+
+    @Inject(method = "getPrecipitation", at = @At("HEAD"), cancellable = true)
+    public void getPrecipitation(CallbackInfoReturnable<Biome.Precipitation> ci) {
+        if (Winternal.enabled) {
+            if (this.getBiomeCategory() != Biome.BiomeCategory.THEEND) {
+                ci.setReturnValue(Biome.Precipitation.SNOW);
+            }
+        }
+    }
+
+    @Inject(method = "warmEnoughToRain", at = @At("HEAD"), cancellable = true)
+    public void warmEnoughToRain(CallbackInfoReturnable<Boolean> ci) {
+        if (Winternal.enabled) {
+            if (this.getBiomeCategory() == Biome.BiomeCategory.OCEAN) {
+                ci.setReturnValue(false);
             }
         }
     }
@@ -25,7 +55,7 @@ public abstract class BiomeMixin {
     @Inject(method = "isHumid", at = @At("HEAD"), cancellable = true)
     public void isHumid(CallbackInfoReturnable<Boolean> ci) {
         if (Winternal.enabled) {
-            if (!this.getBiomeCategory().getName().contains("end")) {
+            if (this.getBiomeCategory() != Biome.BiomeCategory.THEEND) {
                 ci.setReturnValue(true);
             }
         }
